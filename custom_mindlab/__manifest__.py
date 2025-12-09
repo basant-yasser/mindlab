8609c92e27ca1b0823d6d3b00a741c96970145d7
basant <basantyasser567@gmail.com>
2025-12-09 17:45:04 +0200

    update

--- custom_mindlab/__manifest__.py
+++ custom_mindlab/__manifest__.py
@@ -3,8 +3,7 @@
 
     'summary': "Mindlab",
 
-    'description': """
-    Mindlab
+    'description': """Mindlab
     """,
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
